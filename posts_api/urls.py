27721080fddd168ce2043deb767f3b4fe52bddb8
Sam <sam.stearn@gmail.com>
2022-04-21 10:44:28 -0700

    1

--- posts_api/urls.py
+++ posts_api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import PostList, PostDetail, PostListDetailfilter, CreatePost, EditPost, AdminPostDetail, DeletePost
+from .views import PostList, PostDetail
 from . import views
 
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='post_list'), # api/posts will be routed to the PostList view for handling
-    path('<int:pk>', views.PostDetail.as_view(), name='post_detail'), # api/posts will be routed to the PostDetail view for handling
+    path('<int:pk>/', views.PostDetail.as_view(), name='post_detail'), # api/posts will be routed to the PostDetail view for handling
 
 ]
